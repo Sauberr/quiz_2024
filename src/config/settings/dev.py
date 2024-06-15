f8398cf3be185a254859b655853305d271d0b4b5
Toma <greebsasha24@gmail.com>
2024-06-15 20:07:29 +0300

    fix bags, add fixtures, fix github actions.

--- src/config/settings/dev.py
+++ src/config/settings/dev.py
@@ -15,7 +15,7 @@ if os.environ.get("GITHUB_WORKFLOW"):
     DATABASES = {
         "default": {
             "ENGINE": "django.db.backends.postgresql",
-            "NAME": "github_actions",
+            "NAME": "postgres",
             "USER": "postgres",
             "PASSWORD": "postgres",
             "HOST": "0.0.0.0",
