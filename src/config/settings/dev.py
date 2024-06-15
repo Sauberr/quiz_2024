e54553e33be5b72964597fa033c6224a461e5d97
Toma <greebsasha24@gmail.com>
2024-06-15 19:53:04 +0300

    fix bags, add fixtures, fix github actions.

--- src/config/settings/dev.py
+++ src/config/settings/dev.py
@@ -10,16 +10,40 @@ ALLOWED_HOSTS = ["*"]
 
 # MIDDLEWARE += [] # noqa # something add like debugtoolbar middleware
 
-DATABASES = {
-    "default": {
-        "ENGINE": "django.db.backends.postgresql",
-        "NAME": os.environ.get("POSTGRES_DB"),
-        "USER": os.environ.get("POSTGRES_USER"),
-        "PASSWORD": os.environ.get("POSTGRES_PASSWORD"),
-        "HOST": os.environ.get("POSTGRES_HOST"),
-        "PORT": os.environ.get("POSTGRES_PORT"),
-    }
-}
+if os.environ.get("GITHUB_WORKFLOW"):
 
+    DATABASES = {
+        "default": {
+            "ENGINE": "django.db.backends.postgresql",
+            "NAME": "github_actions",
+            "USER": "postgres",
+            "PASSWORD": "postgres",
+            "HOST": "127.0.0.1",
+            "PORT": 5432,
+        }
+    }
+else:
+    DATABASES = {
+        "default_local": {
+            "ENGINE": "django.db.backends.postgresql",
+            "NAME": "my_database",
+            "USER": "postgres",
+            "PASSWORD": "postgres",
+            "HOST": "localhost",
+            "PORT": 5432,
+        },
+        "default": {
+            "ENGINE": "django.db.backends.sqlite3",
+            "NAME": BASE_DIR / "db.sqlite3",
+        },
+        "default_docker": {
+            "ENGINE": "django.db.backends.postgresql",
+            "NAME": os.environ.get("POSTGRES_DB"),
+            "USER": os.environ.get("POSTGRES_USER"),
+            "PASSWORD": os.environ.get("POSTGRES_PASSWORD"),
+            "HOST": os.environ.get("POSTGRES_HOST"),
+            "PORT": os.environ.get("POSTGRES_PORT"),
+        }
+    }
 
 STATIC_URL = "static/"
